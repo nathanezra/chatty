73470a244577fa87aa76edc1346a92fe0842931d
nathanezra <ezrarodrigue@gmail.com>
2021-03-14 21:18:16 +0200

    userInfo store and userActions

--- src/store.js
+++ src/store.js
@@ -1,12 +1,20 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
+import {
+    userSigninReducer,
+    userRegisterReducer,
+  } from './reducers/userReducers';
+
+const userInfo = Cookie.getJSON('userInfo') || null
+
 
 const initialState = {
- 
+    userSignin: { userInfo },
 };
 const reducer = combineReducers({
-
+    userSignin: userSigninReducer,
+    userRegister: userRegisterReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
