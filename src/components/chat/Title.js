2a962a467e3e8a58e923867d57691240fc69ae8e
nathanezra <ezrarodrigue@gmail.com>
2021-03-15 08:48:14 +0200

    title with material ui

--- src/components/chat/Title.js
+++ src/components/chat/Title.js
@@ -1,7 +1,15 @@
-
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
 function Title(props) {
-    return <p >{props.title}</p>
+    return (
+        <Grid container>
+            <Grid item xs={12} >
+                <Typography variant="h5" className="header-message">{props.title}</Typography>
+            </Grid>
+        </Grid>
+    )
   }
 
   
