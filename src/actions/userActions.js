6fc0b85fae4afecd79aa1f39a061bb275eb0fe70
nathanezra <ezrarodrigue@gmail.com>
2021-03-15 12:35:30 +0200

    rigth use of auth

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -10,43 +10,60 @@ import { auth } from '../config/firebase';
 import { db } from '../config/firebase'
 
 
-const signin = (email, password) => async (dispatch, {getFirebase, getFirestore}) => {
-  dispatch({ type: USER_SIGNIN_REQUEST, payload: { email, password } });
-  try {
-    await auth().signInWithEmailAndPassword(email, password);
-    dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
-    Cookie.set('userInfo', JSON.stringify(data));
-  } catch (error) {
+const signin = (email, password) => async (dispatch) => {
+
+
+  dispatch({ type: USER_SIGNIN_REQUEST });
+  auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    let user = userCredential.user;
+    console.log(user.displayName)
+    Cookie.set('userInfo', JSON.stringify({name: user.displayName, onLine: false}));
+    dispatch({ type: USER_SIGNIN_SUCCESS, payload: {name: user.displayName, email: email, password: password, onLine: false}});
+    
+  }).catch((error) => {
+    console.log(error)
     dispatch({ type: USER_SIGNIN_FAIL, payload: error.message });
-  }
+  });
+  // try {
+  //   await auth().signInWithEmailAndPassword(email, password);
+  //   dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
+  //   Cookie.set('userInfo', JSON.stringify(data));
+  // } catch (error) {
+  //   dispatch({ type: USER_SIGNIN_FAIL, payload: error.message });
+  // }
 }
 
 const register = (name, email, password) => async (dispatch) => {
   dispatch({ type: USER_REGISTER_REQUEST, payload: { name, email, password } });
-
-  try{
-    await auth().createUserWithEmailAndPassword(email, password);
-    db.collection('utilisateurs').add({name: name, onLine: false})
-    .then((docRef) => {
-      dispatch({ type: USER_REGISTER_SUCCESS, payload: {name: name, onLine: false}});
-      Cookie.set('userInfo', JSON.stringify({name: name, onLine: false}));
-    })
-    .catch((error) => {
+  auth().createUserWithEmailAndPassword(email, password) 
+    .then((userCredential) => {
+      let user = userCredential.user;
+      user.updateProfile({
+        displayName: name,
+      })
+        dispatch({ type: USER_REGISTER_SUCCESS, payload: {name: name, email: email, password: password, onLine: false}});
+        Cookie.set('userInfo', JSON.stringify({name: name, onLine: false}));
+    }).catch((error) => {
       dispatch({ type: USER_REGISTER_FAIL, payload: error.message });
     });
-  }
-  catch (error) {
-    dispatch({ type: USER_REGISTER_FAIL, payload: error.message });
+
+    // db.collection('utilisateurs').add({name: name, onLine: false})
+    // .then((docRef) => {
+    //   dispatch({ type: USER_REGISTER_SUCCESS, payload: {name: name, onLine: false}});
+    //   Cookie.set('userInfo', JSON.stringify({name: name, onLine: false}));
+    // })
+    // .catch((error) => {
+    //   dispatch({ type: USER_REGISTER_FAIL, payload: error.message });
+    // });
+  
   }
   
 
 
-  // let data = await auth().createUserWithEmailAndPassword(email, password);
+const logout = () => (dispatch) => {
 
-}
-
-const logout = () => (dispatch, {getFirebase, getFirestore}) => {
-  auth().signOut(email, password)
+  auth().signOut()
   Cookie.remove("userInfo");
   dispatch({ type: USER_LOGOUT })
 }
--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -20,7 +20,9 @@ function SigninScreen(props) {
   const [password, setPassword] = useState('');
   const userSignin = useSelector(state => state.userSignin);
   const { loading, userInfo, error } = userSignin;
+
   const dispatch = useDispatch();
+  
   const redirect = props.location.search ? props.location.search.split("=")[1] : '/';
   useEffect(() => {
     if (userInfo) {
