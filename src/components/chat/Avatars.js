bcb324b1297841301ff4e410648918ed2962ac8f
nathanezra <ezrarodrigue@gmail.com>
2021-03-15 09:09:50 +0200

    Avatar online

--- src/components/chat/Avatars.js
+++ src/components/chat/Avatars.js
@@ -4,7 +4,9 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
-
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
+import TextField from '@material-ui/core/TextField';
 
 function Avatars (props) {
 
@@ -12,12 +14,32 @@ function Avatars (props) {
 
     return (
         <List>
-            <ListItem button >
-                    <ListItemIcon>
-                        <Avatar alt="Remy Sharp"/>
-                    </ListItemIcon>
-                    <ListItemText primary="John Wick"></ListItemText>
-            </ListItem>
+            {props.users ? props.users.map(u => 
+            {return(
+                <ListItem button >
+                        <ListItemIcon>
+                            <Avatar />
+                        </ListItemIcon>
+                        <ListItemText primary={u.name}></ListItemText>
+                </ListItem>
+            )}): null}
+            <Divider />
+            <Grid item xs={12} style={{padding: '10px'}}>
+                <TextField id="outlined-basic-email" label="Search" variant="outlined" fullWidth />
+            </Grid>
+            <Divider />
+            <List>
+                {props.users ? props.users.map(u => 
+                {return(
+                    <ListItem button >
+                            <ListItemIcon>
+                                <Avatar />
+                            </ListItemIcon>
+                            <ListItemText primary={u.name}></ListItemText>
+                            <ListItemText secondary="online" align="right"></ListItemText>
+                    </ListItem>
+                )}): null}
+            </List>
         </List>
     )
       
--- src/components/chat/MessageList.js
+++ src/components/chat/MessageList.js
@@ -3,6 +3,14 @@ import Avatars from './Avatars';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Divider from '@material-ui/core/Divider';
+import TextField from '@material-ui/core/TextField';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+
 
 const useStyles = makeStyles({
       
@@ -10,10 +18,13 @@ const useStyles = makeStyles({
       width: '100%',
       height: '80vh'
     },
+    borderRight500: {
+        borderRight: '1px solid #e0e0e0'
+    },
     
   });
 
-  
+
 function MessageList (props) {
 
     const classes = useStyles();
@@ -22,7 +33,10 @@ function MessageList (props) {
     return (
         <>
             <Grid container component={Paper} className={classes.chatSection}>
-                <Avatars/>
+                <Grid item xs={3} className={classes.borderRight500}>
+                    <Avatars users={[{name:'nathan'},{name:'paul'}]}/>
+                </Grid>
+                
             </Grid>
             <ul className="message-list">                 
                 {props.messages.map(message => {
